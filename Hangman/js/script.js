30b00d6a1b04b684d2d2635b23a36215ca76a179
Trija Thebe <66869851+thebe-trija57@users.noreply.github.com>
2020-07-28 20:08:49 -0700

    Add files via upload

---
+++ Hangman/js/script.js
@@ -0,0 +1,33 @@
+var nameList = ['blue', 'red', 'purple', 'green', 'brown'];
+var randomNames = nameList[Math.floor(Math.random() * nameList.length)];
+
+
+var headingDiv = document.createElement('div');
+var title = document.createElement('h1');
+var subTitle = document.createElement('p');
+
+headingDiv.id = 'headingDiv';
+headingDiv.classList.add('headingDiv');
+
+title.innerHTML = 'Hangman';
+title.classList.add('title');
+
+
+subTitle.innerHTML = 'Guess the Word.';
+
+document.body.appendChild(headingDiv);
+headingDiv.appendChild(title);
+headingDiv.appendChild(subTitle);
+
+
+var availableName = randomNames.split(''); 
+var totalLetter = function() {
+    for( var i=0; i < availableName.length; i++ ) {
+        var createDiv = document.createElement('div');
+        createDiv.classList.add('letters');
+        var lettersToFill = document.getElementById('letterBlanks');
+        lettersToFill.appendChild(createDiv);
+    }
+}
+console.log(randomNames);
+totalLetter();
